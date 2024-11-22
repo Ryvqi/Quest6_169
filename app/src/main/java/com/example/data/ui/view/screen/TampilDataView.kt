306d35a63fd3119732ac016bb321a28d46cded3a
Ryvqi <rifqikhabibi03@gmail.com>
2024-11-22 17:40:27 +0700

    membuat file tampil data view

---
+++ app/src/main/java/com/example/data/ui/view/screen/TampilDataView.kt
@@ -0,0 +1,14 @@
+package com.example.data.ui.view.screen
+
+import android.widget.Button
+import androidx.compose.runtime.Composable
+import com.example.data.model.Mahasiswa
+import com.example.data.model.RencanaStudi
+
+@Composable
+fun TampilDataView(
+    mhs: Mahasiswa,
+    krs: RencanaStudi,
+    onClickButton: () -> Unit
+){
+}

58230b574c6d2eae80757403cff3822ec7aa59e2
Ryvqi <rifqikhabibi03@gmail.com>
2024-11-22 15:19:14 +0700

    menambahkan elemen di dalam object

--- app/src/main/java/com/example/data/data/MataKuliah.kt
+++ app/src/main/java/com/example/data/data/MataKuliah.kt
@@ -1,4 +1,12 @@
 package com.example.data.data
 
 object MataKuliah {
+    val option = listOf(
+        "Pemrograman Aplikasi Mobile Lanjut",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vision",
+    )
 }

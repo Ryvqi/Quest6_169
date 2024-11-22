8ce12c2a462a29a27a06b925ffea3d484fbc6b6c
Ryvqi <rifqikhabibi03@gmail.com>
2024-11-22 15:46:36 +0700

    menambahkan outlinedtextfield

--- app/src/main/java/com/example/data/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/data/ui/widget/DynamicSelectedField.kt
@@ -1,6 +1,11 @@
 package com.example.data.ui.widget
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -22,6 +27,20 @@ fun DynamicSelectedField(
     ExposedDropdownMenuBox(
         expanded = expanded,
         onExpandedChange = {expanded = !expanded},
-        modifier = Modifier
-    ) { }
+        modifier = modifier
+    ) {
+        OutlinedTextField(
+            readOnly = true,
+            value = selectedValue,
+            onValueChange = {},
+            label = { Text(text = label) },
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+            },
+            colors = OutlinedTextFieldDefaults.colors(),
+            modifier = Modifier
+                .menuAnchor()
+                .fillMaxWidth()
+        )
+    }
 }

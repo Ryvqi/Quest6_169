a96c5a1560ded524cad0ce92edc275969abd6d48
Ryvqi <rifqikhabibi03@gmail.com>
2024-11-22 15:27:40 +0700

    membuat fungsi DynamicSelectedField

--- app/src/main/java/com/example/data/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/data/ui/widget/DynamicSelectedField.kt
@@ -1,2 +1,14 @@
 package com.example.data.ui.widget
 
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.Lifecycle
+
+fun DynamicSelectedField(
+    selectedValue: String,
+    option: List<String>,
+    label: String,
+    onValueChangedEvent: (String) -> Unit,
+    modifier: Modifier = Modifier
+){
+
+}

3b168bf2c3c7bdff8555698339f752b8daa06316
Ryvqi <rifqikhabibi03@gmail.com>
2024-11-22 18:22:40 +0700

    mengubah : pada private value menjadi =

--- app/src/main/java/com/example/data/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/data/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -4,9 +4,11 @@ import androidx.lifecycle.ViewModel
 import com.example.data.model.RencanaStudi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class RencanaStudyViewModel: ViewModel() {
-    private val _krsState: MutableStateFlow(RencanaStudi())
+    private val _krsState = MutableStateFlow(RencanaStudi())
     val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
 
     fun setMataKuliah(mkPilihan: String){

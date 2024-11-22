86b6d20e1902376439527619828301e3e1b42ab6
Ryvqi <rifqikhabibi03@gmail.com>
2024-11-22 16:20:16 +0700

    membuat file RencanaStudyView

---
+++ app/src/main/java/com/example/data/ui/view/screen/RencanaStudyView.kt
@@ -0,0 +1,13 @@
+package com.example.data.ui.view.screen
+
+import androidx.compose.runtime.Composable
+import com.example.data.model.Mahasiswa
+import com.example.data.ui.view.viewmodel.RencanaStudyViewModel
+
+@Composable
+fun RencanaStudyView(
+    mahasiswa: Mahasiswa,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit
+){
+}

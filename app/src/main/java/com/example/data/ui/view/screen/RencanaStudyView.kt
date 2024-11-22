099ed422ffa039dfad60495b9cbf50173c7905f5
Ryvqi <rifqikhabibi03@gmail.com>
2024-11-22 16:25:16 +0700

    menambahkan 3 variable

--- app/src/main/java/com/example/data/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/data/ui/view/screen/RencanaStudyView.kt
@@ -1,6 +1,10 @@
 package com.example.data.ui.view.screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.example.data.model.Mahasiswa
 import com.example.data.ui.view.viewmodel.RencanaStudyViewModel
 
@@ -10,4 +14,10 @@ fun RencanaStudyView(
     onSubmitButtonClicked: (MutableList<String>) -> Unit,
     onBackButtonClicked: () -> Unit
 ){
+    var chosenDropdown by remember { mutableStateOf("")
+    }
+    var checked by remember { mutableStateOf(false) }
+    var pilihanKelas by remember { mutableStateOf("")
+    }
+    var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
 }

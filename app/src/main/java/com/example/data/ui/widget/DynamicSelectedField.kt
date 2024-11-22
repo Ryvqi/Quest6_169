96670375d547297812e0a0b65f96f67bc1a0046e
Ryvqi <rifqikhabibi03@gmail.com>
2024-11-22 15:37:45 +0700

    menambahkan dropdownmenubox

--- app/src/main/java/com/example/data/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/data/ui/widget/DynamicSelectedField.kt
@@ -1,8 +1,15 @@
 package com.example.data.ui.widget
 
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.Lifecycle
 
+@Composable
 fun DynamicSelectedField(
     selectedValue: String,
     option: List<String>,
@@ -10,5 +17,11 @@ fun DynamicSelectedField(
     onValueChangedEvent: (String) -> Unit,
     modifier: Modifier = Modifier
 ){
+    var expanded by remember { mutableStateOf(false) }
 
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = {expanded = !expanded},
+        modifier = Modifier
+    ) { }
 }
